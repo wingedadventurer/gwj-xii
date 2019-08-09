74cb660862c58898be66c1a06031adf96f513ca0
wingedadventurer <wingedadventurer@gmail.com>
2019-08-09 20:59:04 +0200

    Gitignore .import folder

--- godot-project/scripts/tile_dirt.gd
+++ godot-project/scripts/tile_dirt.gd
@@ -1,10 +1,13 @@
 extends Spatial
 
+var object = null
+var has_object := false
+
 func _ready() -> void:
 	$highlight_mesh.material_override = $highlight_mesh.material_override.duplicate()
 
 func _on_area_mouse_entered() -> void:
-	$highlight_animation.play("highlight")
+	$highlight_animation.play("tile_highlight")
 
 func _on_area_mouse_exited() -> void:
-	$highlight_animation.play("unhighlight")
+	$highlight_animation.play("tile_unhighlight")
