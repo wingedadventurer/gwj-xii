3f179ee9747526b7e6ac2fc86042ef68722a1911
wingedadventurer <wingedadventurer@gmail.com>
2019-08-09 18:48:02 +0200

    add test materials and tiles

--- godot-project/scripts/level.gd
+++ godot-project/scripts/level.gd
@@ -1,4 +1,10 @@
 extends Spatial
 
-func _ready():
-	pass
+var scene_camera = preload("res://scenes/camera.tscn")
+
+func _ready() -> void:
+	instance_camera()
+
+func instance_camera() -> void:
+	var camera = scene_camera.instance()
+	add_child(camera)
