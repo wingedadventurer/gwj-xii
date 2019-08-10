964234a55b2796a75a82dc33e780dc03fcede042
wingedadventurer <wingedadventurer@gmail.com>
2019-08-10 20:19:45 +0200

    Add carrot cursor

--- godot-project/scripts/level.gd
+++ godot-project/scripts/level.gd
@@ -2,8 +2,11 @@ extends Spatial
 
 var scene_camera = preload("res://scenes/camera.tscn")
 
+var cursor_image = preload("res://textures/cursor_carrot.png")
+
 func _ready() -> void:
 	instance_camera()
+	Input.set_custom_mouse_cursor(cursor_image)
 
 func instance_camera() -> void:
 	var camera = scene_camera.instance()
