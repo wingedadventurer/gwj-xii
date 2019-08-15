1aec84fe3dc003b2c513c7eee613c146d1cfd9f1
wingedadventurer <wingedadventurer@gmail.com>
2019-08-15 17:02:45 +0200

    Make signal free itself when under game

--- godot-project/scripts/signal.gd
+++ godot-project/scripts/signal.gd
@@ -4,3 +4,7 @@ class_name class_signal
 func _process(delta : float) -> void:
 	# rotate projectile in movement direction
 	pass
+	
+	# check if under certain height
+	if global_transform.origin.y <= -1.0:
+		queue_free()
