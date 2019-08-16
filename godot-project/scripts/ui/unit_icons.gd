9e85396bfcd4a2aaa97cf5695fefffa182952157
wingedadventurer <wingedadventurer@gmail.com>
2019-08-16 16:25:21 +0200

    Reorganize game ui; added unit control icons

--- godot-project/scripts/ui/unit_icons.gd
+++ godot-project/scripts/ui/unit_icons.gd
@@ -2,20 +2,18 @@ extends PanelContainer
 
 onready var scene_unit_icon = preload("res://scenes/ui/unit_icon.tscn")
 
-onready var list_carrots = get_node("vb/carrots")
-onready var list_potatoes = get_node("vb/potatoes")
-onready var list_onions = get_node("vb/onions")
+onready var list_units = get_node("vb/units")
 
 func _ready() -> void:
 	fill_lists()
 
 func fill_lists() -> void:
 	for carrot in get_tree().get_nodes_in_group("carrot"):
-		add_icon(list_carrots, carrot)
+		add_icon(list_units, carrot)
 	for potato in get_tree().get_nodes_in_group("potato"):
-		add_icon(list_potatoes, potato)
+		add_icon(list_units, potato)
 	for onion in get_tree().get_nodes_in_group("onion"):
-		add_icon(list_onions, onion)
+		add_icon(list_units, onion)
 
 func add_icon(list, unit) -> void:
 	var unit_icon = scene_unit_icon.instance()
