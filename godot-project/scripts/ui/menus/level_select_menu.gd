9517af2dc198d06a429ddf395ff94fb60b743391
wingedadventurer <wingedadventurer@gmail.com>
2019-08-18 02:28:15 +0200

    Add all level names

--- godot-project/scripts/ui/menus/level_select_menu.gd
+++ godot-project/scripts/ui/menus/level_select_menu.gd
@@ -3,14 +3,17 @@ extends Control
 onready var grid_container := $grid_container
 onready var tween := $tween
 
+var out_position = Vector2(-1000, 127)
+var in_position = Vector2(320, 127)
+
 func appear() -> void:
-	tween.interpolate_property(grid_container, "rect_position", Vector2(-1000, 127), Vector2(228, 127), 1.0, Tween.TRANS_BOUNCE, Tween.EASE_OUT)
+	tween.interpolate_property(grid_container, "rect_position", out_position, in_position, 0.8, Tween.TRANS_BACK, Tween.EASE_OUT)
 	tween.start()
 	yield(get_tree().create_timer(0.01), "timeout") # I know, I know
 	visible = true
 
 func disappear() -> void:
-	tween.interpolate_property(grid_container, "rect_position", Vector2(228, 127), Vector2(-1000, 127), 0.5, Tween.TRANS_CUBIC, Tween.EASE_IN)
+	tween.interpolate_property(grid_container, "rect_position", in_position, out_position, 0.5, Tween.TRANS_CUBIC, Tween.EASE_IN)
 	tween.start()
 	yield(tween, "tween_all_completed")
 	visible = false
